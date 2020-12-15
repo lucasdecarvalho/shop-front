20ae580550fbb706e79b3f4768a5fc9f93ad717c
Lucas de Carvalho <contato@lucasdecarvalho.com.br>
2020-12-15 11:10:52 -0300

    dash update

--- src/app/modules/sellers/components/dashboard/dashboard.component.ts
+++ src/app/modules/sellers/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SellersService } from '../../sellers.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  firstName: string;
+
+  constructor(private seller: SellersService) { }
 
   ngOnInit(): void {
 
-  }
+    this.seller.sellerData()
+      .subscribe(response => {
 
+        // @ts-ignore
+        let company = response.seller;
+
+        // @ts-ignore
+        this.firstName = company.firstName;
+
+          // fantasia: this.company.seller.fantasia,
+          // nome: this.company.seller.nome,
+          // abertura: this.company.seller.abertura,
+          // logradouro: this.company.seller.logradouro,
+          // numero: this.company.seller.numero,
+          // complemento: this.company.seller.complemento,
+          // bairro: this.company.seller.bairro,
+          // municipio: this.company.seller.municipio,
+          // uf: this.company.seller.uf,
+          // cep: this.company.seller.cep,
+          // telefone: this.company.seller.telefone,
+          // telefone2: this.company.seller.telefone2,
+          // alias: this.company.seller.alias,
+          // firstName: this.company.seller.firstName,
+          // lastName: this.company.seller.lastName,
+          // cpf: this.company.seller.cpf,
+          // cel: this.company.seller.cel,
+          // bankName: this.company.seller.bankName,
+          // bankType: this.company.seller.bankType,
+          // bankAg: this.company.seller.bankAg,
+          // bankAccount: this.company.seller.bankAccount,
+      });
+  }
 }
