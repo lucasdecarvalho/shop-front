8c721415c362827d407c767e4409b1cf8bd64b38
Lucas de Carvalho <contato@lucasdecarvalho.com.br>
2021-01-14 17:25:00 -0300

    about + terms components

--- src/app/modules/store/store-routing.module.ts
+++ src/app/modules/store/store-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StoreComponent } from './store.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { TermsComponent } from './components/terms/terms.component';
 
 const routes: Routes = [
   {
@@ -17,7 +19,28 @@ const routes: Routes = [
               component: HomeComponent,
             }
         ]
-  }];
+  },
+  { 
+    path: 'sobre-nos',
+    component: StoreComponent,
+        children: [
+            {
+              path: '',
+              component: AboutUsComponent,
+            }
+        ],
+  },
+  { 
+    path: 'termos-de-uso',
+    component: StoreComponent,
+        children: [
+            {
+              path: '',
+              component: TermsComponent,
+            }
+        ],
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/store/store.module.ts
+++ src/app/modules/store/store.module.ts
@@ -7,12 +7,18 @@ import { SharedModule } from '../../shared/shared.module';
 import { HomeComponent } from './components/home/home.component';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { CompanyComponent } from './components/company/company.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { TermsComponent } from './components/terms/terms.component';
 
 @NgModule({
   declarations: [
     StoreComponent,
     HomeComponent,
     CompanyComponent,
+    AboutusComponent,
+    AboutUsComponent,
+    TermsComponent,
   ],
   imports: [
   CommonModule,
