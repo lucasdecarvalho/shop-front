acf15ff8d606018dd485fd296b2f87759cdd82ac
Lucas de Carvalho <contato@lucasdecarvalho.com.br>
2021-02-11 14:52:33 -0300

    name and alias dashboard

--- src/app/modules/sellers/components/dashboard/dashboard.component.ts
+++ src/app/modules/sellers/components/dashboard/dashboard.component.ts
@@ -5,6 +5,8 @@ import { AuthGuard } from 'src/app/core/guards/auth.guard';
 import { Router } from '@angular/router';
 import Swal from "sweetalert2";
 import { Title } from '@angular/platform-browser';
+import { isObject } from 'util';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,12 +16,13 @@ import { Title } from '@angular/platform-browser';
 export class DashboardComponent implements OnInit {
 
   firstName: string;
+  alias: string;
   products: any;
   id: number;
 
   constructor(
     private title: Title,
-    private seller: SellersService, 
+    private seller: SellersService,
     private storeServive: StoreService, 
     private router: Router) { 
 
@@ -28,7 +31,13 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
 
-    }
+    this.seller.sellerData()
+      .subscribe(data => {
+
+        this.firstName = data['seller']['firstName'];
+        this.alias = data['seller']['alias'];
+    });
+  }
 
     logout() {
       window.localStorage.removeItem('token');
